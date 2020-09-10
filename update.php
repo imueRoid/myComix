137f73837d9867b0ab670e77c1c3c5845ac93b79
imueRoid <70838986+imueRoid@users.noreply.github.com>
2020-09-10 14:22:39 +0900

    Add files via upload
    자동업데이트 추가

--- update.php
+++ update.php
@@ -2,54 +2,51 @@
 $version = json_decode(file_get_contents("version.json"), true);
 $server_version =  json_decode(file_get_contents("https://raw.githubusercontent.com/imueRoid/myComix/master/version.json"), true); 
 
-if($server_version['index'] > $version['index']) {
-	$new_index = file_get_contents("https://raw.githubusercontent.com/imueRoid/myComix/master/index.php");
-	if($new_index === false) {
-		echo "새 index.php 파일을 가져오는데 실패했습니다.<br>";
-	} else {	
-		if(!unlink("./index.php")){
-			echo "기존 index.php 파일의 삭제에 실패했습니다. 권한을 확인해주세요.<br>";
-		} else {
-			file_put_contents("./index.php", $new_index);
-			echo $version['index']."에서 ".$server_version['index']."로 프로그램(index.php)의 업데이트가 완료되었습니다.";
-		}
-	}
-	$newversion_json =  file_get_contents("https://raw.githubusercontent.com/imueRoid/myComix/master/version.json"); 
-	if($newversion_json === false) {
-		echo "새 version_json 파일을 가져오는데 실패했습니다.<br>";
-	} else {	
-		if(!unlink("./version.json")){
-			echo "기존 config.json 파일의 삭제에 실패했습니다. 권한을 확인해주세요.<br>";
-		} else {
-			file_put_contents("./version.json", $newversion_json);
-			echo "인덱스 버전".$version['index']."에서 ".$server_version['index']."로 업데이트가 완료되었습니다.<br>";
-		}
-	}
-}
+#if($server_version['index'] > $version['index'] || $server_version['viewer'] > $version['viewer']) {
+	echo "업데이트파일을 가져옵니다.<br><br>";
 
-if($server_version['viewer'] > $version['viewer']) {
-	$new_viewer = file_get_contents("https://raw.githubusercontent.com/imueRoid/myComix/master/viewer.php");
-	if($new_viewer === false) {
-		echo "새 viewer.php 파일을 가져오는데 실패했습니다.<br>";
-	} else {	
-		if(!unlink("./viewer.php")){
-			echo "기존 viewer.php 파일의 삭제에 실패했습니다. 권한을 확인해주세요.<br>";
+	$new_file = file_get_contents("https://github.com/imueRoid/myComix/archive/master.zip");
+
+	if($new_file === false) {
+		echo "업데이트파일을 가져오는데 실패했습니다.<br>";
+		echo "잠시 후 다시 시도해주세요.<br>";
+	} else {
+		echo "업데이트 파일 용량: ".strlen($new_file)."byte<br>";
+		$write_file = file_put_contents("./update.zip", $new_file);
+		echo "기록한 파일 용량: ".$write_file."byte<br>";
+		if($write_file != strlen($new_file))  {
+			echo "업데이트파일을 서버에 기록하는데 실패했습니다.<br>";
+			echo "잠시 후 다시 시도해주세요.<br>";
 		} else {
-			file_put_contents("./viewer.php", $new_viewer);
-			echo $version['viewer']."에서 ".$server_version['viewer']."로 프로그램(viewer.php)의 업데이트가 완료되었습니다.<br>";
+			$zip = new ZipArchive;
+			if ($zip->open('update.zip') === TRUE) {
+				for($i = 1; $i < $zip->numFiles; $i++) {
+					$file_name = str_replace($zip->getNameIndex(0), "", $zip->getNameIndex($i));
+					if(is_file("config.php") && $file_name == "config.php") {
+						echo "config.php 파일은 교체하지 않습니다.<br>";
+					} elseif(is_file("update.php") && $file_name == "update.php") {
+						echo "update.php 파일은 교체하지 않습니다.<br>";
+					} else {
+						$update_temp = $zip->getFromIndex($i);
+						echo $file_name."파일 용량: ".strlen($update_temp)."byte, ";
+						$write_file = file_put_contents($file_name, $update_temp);
+						echo "기록한 파일 용량: ".$write_file."byte";
+						if($write_file == strlen($update_temp)){
+							echo " -------------- OK <br>";
+						}
+					}
+				}
+					$zip->close();
+					unlink("update.zip");
+			} else {
+				echo '업데이트 파일의 압축해제에 실패했습니다. 잠시 후 다시 시도해주세요.<br>';
+			}
 		}
 	}
-	$newversion_json =  file_get_contents("https://raw.githubusercontent.com/imueRoid/myComix/master/version.json"); 
-	if($newversion_json === false) {
-		echo "새 version_json 파일을 가져오는데 실패했습니다.<br>";
-	} else {	
-		if(!unlink("./version.json")){
-			echo "기존 config.json 파일의 삭제에 실패했습니다. 권한을 확인해주세요.<br>";
-		} else {
-			file_put_contents("./version.json", $newversion_json);
-			echo "뷰어 버전".$version['viewer']."에서 ".$server_version['viewer']."로 업데이트가 완료되었습니다.<br>";
-		}
-	}
-}
+	
+		
+#} if($server_version['index'] < $version['index'] && $server_version['viewer'] < $version['viewer']) {
+#	echo "업데이트가 필요하지 않습니다.<br>";
+#}
 echo "<br><br><a href=index.php>홈으로 돌아갑니다.</a>"
 ?>
