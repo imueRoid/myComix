b12c0127bb1d636f0a40a9370a0a75e9b2490392
imueRoid <70838986+imueRoid@users.noreply.github.com>
2020-09-20 11:25:35 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -52,41 +52,37 @@ $jpgcounter = 0;
 $jpg_list = array();
 $dir_list = array();
 $title_list = array();
+$dirinfo = array();
+if(str_replace("/", "", $base_dir) == str_replace("/", "", $dir)){
+	$is_root = true;
+}
 foreach ($iterator as $fileinfo) {
 	$fileis_remote = "c";
-	if($base_dir == $dir) {
-		$rootdir = array();
-		$getmodefile = $base_dir."/".$fileinfo.".json";
+	if($is_root == true) {
+		$getmodefile = $base_dir."/".$fileinfo->getFilename().".json";
 		if(is_file($getmodefile) == true) {
 			$dirmode_arr = array();
 			$dirmode_arr = json_decode(file_get_contents($getmodefile), true);
 			if($dirmode_arr[$_SESSION["user_group"]] !== 1) {
 				continue;
-			}
-			if($dirmode_arr['remote'] == "n" || $dirmode_arr['remote'] == null){
-				$fileis_remote = "n";
-			} else {
+			} elseif($dirmode_arr['remote'] == "y"){
 				$fileis_remote = "y";
 			}
 		}
 	}
-
     if (!$fileinfo->isDot() && $fileinfo != "@eaDir" && $fileinfo->isDir()) {
-		if(strpos($dir, "rclone_") !== false || $is_remote == "y") {
+		if(strpos($dir, "rclone_") !== false || $is_remote == "y" || strpos($fileinfo->getFilename(), "rclone_") !== false || $fileis_remote == "y") {
 			$dir_list[$dircounter] = $fileinfo->getFilename();
+			$dirinfo[$fileinfo->getFilename()] = "remote";
 			$dircounter++;
 		} else {
-			if(strpos($fileinfo->getFilename(), "rclone_") !== false || $fileis_remote == "y") {
-				$sub_dircounter = 1;
-			} else {
-				$sub_iterator = new DirectoryIterator($dir."/".$fileinfo->getFilename());
-				$sub_dircounter = 0;
-				$subdir_list = array();
-				foreach ($sub_iterator as $subfileinfo) {
-					if (!$subfileinfo->isDot() && $subfileinfo != "@eaDir" && $subfileinfo->isDir()) {
-						$sub_dircounter++;
-						break;
-					}
+			$sub_iterator = new DirectoryIterator($dir."/".$fileinfo->getFilename());
+			$sub_dircounter = 0;
+			$subdir_list = array();
+			foreach ($sub_iterator as $subfileinfo) {
+				if (!$subfileinfo->isDot() && $subfileinfo != "@eaDir" && $subfileinfo->isDir()) {
+					$sub_dircounter++;
+					break;
 				}
 			}
 			if($sub_dircounter>0){
@@ -208,7 +204,7 @@ for($count=0;$count < count($bookmark_arr); $count++){
 <div class="row row-cols-1 row-cols-md-2">
 
 	<?php
-	if($dir == $base_dir) {
+	if($is_root == true) {
 	} else {
 		$nowdirarr = explode("/", $getdir);
 		$temp = count($nowdirarr);
@@ -243,18 +239,19 @@ for($count=0;$count < count($bookmark_arr); $count++){
 	<a href='index.php?dir=<?php echo encode_url($getdir."/".$fileinfo);?>'>
     <div class="card border-secondary m-1 p-0">
 				<div class="card-body text-secondary m-1 p-1">
+				
 <?php
-if(strpos($fileinfo, "rclone_") !== false || strpos($dir, "rclone_") !== false || $is_remote == "y"){
+if($dirinfo[$fileinfo] == "remote"){
 ?>	
 					<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-hdd-network-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h5.5v3A1.5 1.5 0 0 0 6 11.5H.5a.5.5 0 0 0 0 1H6A1.5 1.5 0 0 0 7.5 14h1a1.5 1.5 0 0 0 1.5-1.5h5.5a.5.5 0 0 0 0-1H10A1.5 1.5 0 0 0 8.5 10V7H14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
-</svg> <?php echo str_replace("rclone_", "", $fileinfo); ?>
+					  <path fill-rule="evenodd" d="M2 2a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h5.5v3A1.5 1.5 0 0 0 6 11.5H.5a.5.5 0 0 0 0 1H6A1.5 1.5 0 0 0 7.5 14h1a1.5 1.5 0 0 0 1.5-1.5h5.5a.5.5 0 0 0 0-1H10A1.5 1.5 0 0 0 8.5 10V7H14a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm.5 3a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2 0a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+					</svg> <?php echo str_replace("rclone_", "", $fileinfo); ?>
 <?php
 } else {
 ?>	
 					<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
-</svg> <?php echo $fileinfo;?>
+					  <path fill-rule="evenodd" d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
+					</svg> <?php echo $fileinfo;?>
 <?php
 }
 ?>
@@ -312,7 +309,7 @@ if(strpos($fileinfo, "rclone_") !== false || strpos($dir, "rclone_") !== false |
 							$configfile = substr($zip_file, 0, strpos(strtolower($zip_file), ".cbz")).".json";
 						}
 
-	if(strpos($dir, "rclone_") !== false || $is_remote == "y") {
+	if($is_remote == "y") {
 		if(is_File($configfile) === false){
 			$img_output = $null_image;
 			$totalpage = "nodata-";
