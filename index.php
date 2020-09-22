7646093f65573d55e34122597b2f9aa25b26bc59
imueRoid <70838986+imueRoid@users.noreply.github.com>
2020-09-22 21:07:40 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -321,7 +321,7 @@ if($dirinfo[$fileinfo] == "remote"){
 		$zip_file = str_replace("_imgfolder","", $zip_file);
 		$configfile = $zip_file."/image_files.json";
 					if(is_File($configfile) === false){
-						$jpg_cover = file_get_contents($zip_file."/".$jpg_folder[$fileinfo][0]);
+						$jpg_cover = file_get_contents($zip_file."/".$jpg_folder[str_replace("_imgfolder","", $fileinfo)][0]);
 						$size = getimagesizefromstring($jpg_cover);
 						if($size[0] > $size[1]) {
 							$x_point = ($size[0]/2) - $size[1];
@@ -400,8 +400,7 @@ if($dirinfo[$fileinfo] == "remote"){
 							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-file-earmark-image" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M12 16a2 2 0 0 0 2-2V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8zM3 2a1 1 0 0 1 1-1h5.5v2A1.5 1.5 0 0 0 11 4.5h2V10l-2.083-2.083a.5.5 0 0 0-.76.063L8 11 5.835 9.7a.5.5 0 0 0-.611.076L3 12V2z"/>
   <path fill-rule="evenodd" d="M6.502 7a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
-</svg> <?php echo str_replace("_imgfolder","", $fileinfo);
-$fileinfo; ?>
+</svg> <?php echo str_replace("_imgfolder","", $fileinfo); ?>
 						</div>
 					</div>
 				  </div>
