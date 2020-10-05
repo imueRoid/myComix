69f8fec72146209d7d3eddfdd0d043efcbb7eea0
imueRoid <70838986+imueRoid@users.noreply.github.com>
2020-10-05 16:23:38 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -351,14 +351,17 @@ if($use_listcover == "y"){
 			if(strpos($file, "json") !== false || $file == "." || $file == ".." || $file == "@eaDir"){
 			} elseif (strpos(strtolower($file), "zip") !== false || strpos(strtolower($file), "cbz") !== false || strpos(strtolower($file), "rar") !== false || strpos(strtolower($file), "cbr") !== false || strpos(strtolower($file), "pdf") !== false || is_file($dir."/".$file_marker."/".$file."/image_files.json") == true) {
 				$totalfile[] = $file;
+				if($recent[$getdir."/".$file_marker] == null) {
+					echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
+				} else {
+					$new_marker_od = array_search ($recent[$getdir."/".$file_marker], $totalfile);
+					if(count($totalfile) > ($new_marker_od + 1)) {
+						echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
+					}
+				}
 			}
 		}
-		$new_marker_od = array_search ($recent[$getdir."/".$file_marker], $totalfile);
-		if(count($totalfile) > ($new_marker_od + 1)) {
-			echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
-		}
 		unset($totalfile);
-
 ?>
 					<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 					  <path fill-rule="evenodd" d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a1.99 1.99 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3zm-8.322.12C1.72 3.042 1.95 3 2.19 3h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139z"/>
@@ -403,11 +406,15 @@ if($use_listcover == "y"){
 			if(strpos($file, "json") !== false || $file == "." || $file == ".." || $file == "@eaDir"){
 			} elseif (strpos(strtolower($file), "zip") !== false || strpos(strtolower($file), "cbz") !== false || strpos(strtolower($file), "rar") !== false || strpos(strtolower($file), "cbr") !== false || strpos(strtolower($file), "pdf") !== false || is_file($dir."/".$file_marker."/".$file."/image_files.json") == true) {
 				$totalfile[] = $file;
+				if($recent[$getdir."/".$file_marker] == null) {
+					echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
+				} else {
+					$new_marker_od = array_search ($recent[$getdir."/".$file_marker], $totalfile);
+					if(count($totalfile) > ($new_marker_od + 1)) {
+						echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
+					}
+				}
 			}
-		}
-		$new_marker_od = array_search ($recent[$getdir."/".$file_marker], $totalfile);
-		if(count($totalfile) > ($new_marker_od + 1)) {
-			echo "<div class=\"card-img-overlay m-1 p-0 text-right\"><span class=\"badge badge-pill badge-warning\">new</span></div>";
 		}
 		unset($totalfile);
 
